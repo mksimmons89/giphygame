26b8eaef6bda16567dff563fc5c73c2eb62da54d
Mary Simmons <MarySimmons@Marys-MacBook-Pro.local>
2018-01-06 14:33:43 -0700

    added rating

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -45,10 +45,43 @@ $.ajax({
   // console.log(Ratings);
   // document.write(response.Ratings[1].Source);
 for (var i = 0; i <response.data.length; i++){
-  console.log(response.data[i].title);
-  $(".coolGifs").append("<img src='" + response.data[i].images.fixed_height_small.url + "'/>");
+var newImg = $("<img class='giphy' />");
+var rating = response.data[i].rating;
+var p = $('<h6>').text("Ratings: " + rating);
+
+newImg.attr("data-state", "still")
+  .attr("data-still", response.data[i].images.fixed_height_small_still.url)
+  .attr("data-anim", response.data[i].images.fixed_height_small.url)
+  .attr("src", response.data[i].images.fixed_height_small_still.url)
+// then, the last thing we do is we append the newImg to the coolGifs container
+$(".coolGifs").append(newImg).append(p);
+
+
 }
 
 });
 
 });
+
+$(".coolGifs").on("click", ".giphy", function() {
+	// clear the coolGifs container first!!!
+	var gifState = $(this).attr("data-state");
+	var animUrl = $(this).attr("data-anim");
+	var stillUrl = $(this).attr("data-still");
+
+	if (gifState == "still") {
+		$(this).attr("src", animUrl); // this sets the src attribute to the animated url
+		// set 'data-state' to be equal to 'anim'
+	} else {
+		// set src to be equal to value to data-still
+		// set 'data-state' to be equal to still
+	}
+
+});
+
+// Add a click event for the submit button in the 'add animal' section
+// This click event should call the 'createNewButton' function
+
+// this is calling it initially --- you'll be calling it again
+// each time a new button is created
+createButtons();
